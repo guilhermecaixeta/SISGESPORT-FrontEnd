3f1a9bbf19180c145920f1c8f2b218ce40d8d1bf
Guilherme Lúcio <guilhermecaixeta@users.noreply.github.com>
2018-07-16 08:55:24 -0300

    Correção rotas api
    Correção de acesso as rotas da api SISGESPORT-Backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiEndPoint: 'https://sisgesport-backend.herokuapp.com/',
+  apiEndPoint: 'https://sisgesport-backend.herokuapp.com/api/',
   apiEndPointLogin: 'https://sisgesport-backend.herokuapp.com/auth'
 };
