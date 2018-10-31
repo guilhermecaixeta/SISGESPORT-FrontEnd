fd2337fdd1e72199a1d7a69a4866dcffca5d44d9
Guilherme Lúcio <guilherme.lpcaixeta@gmail.com>
2018-10-31 00:38:08 -0300

    Correção routerTransiction e Internacionalização

--- src/app/layout-sistema/esporte/penalidade/penalidade-crud/penalidade-crud.component.ts
+++ src/app/layout-sistema/esporte/penalidade/penalidade-crud/penalidade-crud.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { BaseCrudComponent } from '../../../../base';
 import { Validators } from '@angular/forms';
 import { Penalidade } from '../../../../model/penalidade.model';
+import { routerTransition } from '../../../../router.animations';
 
 @Component({
   selector: 'app-penalidade-crud',
   templateUrl: './penalidade-crud.component.html',
-  styleUrls: ['./penalidade-crud.component.scss']
+  styleUrls: ['./penalidade-crud.component.scss'],
+  animations: [routerTransition()]
 })
 export class PenalidadeCrudComponent extends BaseCrudComponent {
   value: boolean = false;
@@ -21,5 +23,5 @@ export class PenalidadeCrudComponent extends BaseCrudComponent {
   finalizar() {
     let penalidade = new Penalidade(this.formulario.value);
     this.Persistir<Penalidade>(penalidade);
-   }
+  }
 }
