92cefe743927d5cbba4abea885246d4d2ce6a86b
Guilherme Lúcio <guilhermecaixeta@users.noreply.github.com>
2018-11-07 11:13:23 -0200

    Correção há token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
@@ -6,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-    constructor() {
+    constructor(private router: Router) {
     }
 
     ngOnInit() {
+       let token = localStorage.get('token');
+       if(token){
+        this.router.navigate(['/principal'], { relativeTo: this.activatedRoute });
+       }else{
+        this.router.navigate(['/login'], { relativeTo: this.activatedRoute });
+       }    
     }
 }
